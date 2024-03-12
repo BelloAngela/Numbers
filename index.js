6f5c349be7bbf835533e904ab51266d0118cdb0b
BelloAngela <bellotile@mail.com>
2024-03-11 21:16:10 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,85 @@
+// Complete the Numbers class below
+// the constructor has already been provided
+class Numbers {
+  constructor(data) {
+    //data can either be a string or an array of numbers
+    if (typeof data === "string") {
+      this.data = str.split(",").map((number) => number * 1);
+    } else {
+      this.data = data;
+    }
+  }
+  count() {
+    //return the count of numbers in data
+    return this.data.length;
+  }
+  printNumbers() {
+    //print the numbers in data
+    this.data.forEach((element, index) => {
+      console.log(element, index);
+  });
+}
+  odds() {
+    //return the odd numbers in data
+    const oddsArray = [];
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i] %2 > 0) {
+        oddsArray.push(this.data[i]);
+  }
+    }
+    return oddsArray;
+  }
+  evens() {
+    //return the even numbers in data
+  const evensArray = [];
+  for (let i = 0; i < this.this.data.length; i++) {
+    if (this.data[i] %2 == 0) {
+    evensArray.push(this.data[i]);
+    }
+  } 
+  return evensArray;
+}
+  sum() {
+    //return the sum of the numbers
+    return this.data.reduce((acc, num) => {
+      const newSum = acc + num;
+      return newSum;
+    }, 0);
+  }
+  product() {
+    //return the product of the numbers
+    return this.data.reduce((acc, num) => {
+      const newProduct = acc * num;
+      return newProduct;
+    }, 1); 
+  }
+  greaterThan(target) {
+    //return the numbers greater than the target
+    const result = this.data.filter(num => num > target);
+    return result;
+  }
+  howMany(target) {
+    //return the count of a given number
+  let frequency = 0;
+  this.data.forEach((item) => {
+    if (item == target) {
+      frequency += 1;
+    }
+  });
+  return 'The number represented by ${target} appears ${frequency} times'; 
+ }
+}
+
+//Prompt the user for a list of integers separated by commas
+const str = prompt("enter some numbers, like this", "1,2,3,3,5,9");
+
+//create an instance of numbers
+const n1 = new Numbers(str);
+console.log(n1.count()); //returns count of numbers
+n1.printNumbers(); //prints the number along with their indexes
+console.log(n1.odds()); //returns odd numbers
+console.log(n1.evens()); //returns even numbers
+console.log(n1.sum()); //returns sum of numbers
+console.log(n1.product()); //returns product of numbers
+console.log(n1.greaterThan(3)); //returns numbers greater than another number
+console.log(n1.howMany(3)); //return the count of a specific number
